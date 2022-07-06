6db18f628422bef0c13b8228dc32fa2eb407c4fc
boner.engineer <boner.engineer@outlook.jp>
2022-07-06 10:42:17 +0900

    Animation add

--- src/resources/js/Layouts/Main.jsx
+++ src/resources/js/Layouts/Main.jsx
@@ -3,6 +3,10 @@ import { Fragment } from 'react'
 import { Popover, Transition } from '@headlessui/react'
 import { MenuIcon, XIcon } from '@heroicons/react/outline'
 
+import "animate.css/animate.min.css";
+
+import { AnimationOnScroll } from 'react-animation-on-scroll';
+
 const navigation = [
   { name: 'Product', href: '#' },
   { name: 'Features', href: '#' },
@@ -12,7 +16,8 @@ const navigation = [
 
 export default function Main() {
   return (
-    <div className="relative bg-white overflow-hidden py-40">
+    <>
+    <AnimationOnScroll className="relative bg-white overflow-hidden py-40" animateIn="animate__fadeIn">
       <div className=" mx-auto">
         <div className="relative z-10 pb-8 bg-white sm:pb-16 md:pb-20 lg:max-w-2xl lg:w-full lg:pb-28 xl:pb-32">
           <svg
@@ -39,12 +44,12 @@ export default function Main() {
       <div className="lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
         <img
           className="h-56 w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-full"
-          src="https://images.unsplash.com/photo-1551434678-e076c223a692?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2850&q=80"
+          src="https://images.unsplash.com/photo-1519389950473-47ba0277781c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2850&q=80"
           alt=""
         />
       </div>
+    </AnimationOnScroll>
 
-      
-    </div>
+    </>
   )
 }
