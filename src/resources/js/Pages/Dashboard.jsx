3c31c0705d2431f20abb6ad1ba89db684441eb03
boner.engineer <boner.engineer@outlook.jp>
2022-07-15 15:38:16 +0900

    変更、削除ボタン追加

--- src/resources/js/Pages/Dashboard.jsx
+++ src/resources/js/Pages/Dashboard.jsx
@@ -1,8 +1,15 @@
 import React from 'react';
 import Authenticated from '@/Layouts/Authenticated';
 import { Head } from '@inertiajs/inertia-react';
+import { Link } from "@inertiajs/inertia-react";
 
 export default function Dashboard(props) {
+    const handleDelete = (id) => {
+        destory(route("user.destroy", id), {
+            preserveScroll: true,
+        });
+    };
+    console.log(props.auth.user)
     return (
         <Authenticated
             auth={props.auth}
@@ -21,6 +28,8 @@ export default function Dashboard(props) {
                                 <th className="px-4 py-2">名前</th>
                                 <th className="px-4 py-2">e-Mail</th>
                                 <th className="px-4 py-2">電話番号</th>
+                                <th className="px-4 py-2">更新</th>
+                                <th className="px-4 py-2">削除</th>
                                 </tr>
                             </thead>
                             <tbody>
@@ -31,10 +40,45 @@ export default function Dashboard(props) {
                                 </tr>
                             </tbody>
                             </table>
+                            <Link href={route("user.edit", props.auth.user.id)}>
+                                <button className="px-4 py-2 bg-green-500 text-white rounded-lg text-xs font-semibold">
+                                    更新
+                                </button>
+                            </Link>
+                            <button className="px-4 py-2 bg-red-500 text-white rounded-lg text-xs font-semibold"
+                                onClick={() => handleDelete(props.auth.user.id)
+                            }>
+                                削除
+                            </button>
                         </div>
                     </div>
                 </div>
             </div>
+
+            <div className="py-12">
+                <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                    <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                        <div className="p-6 bg-white border-b border-gray-200">
+                        <table className="table-auto">
+                            <thead>
+                                <tr>
+                                <th className="px-4 py-2">投稿日</th>
+                                <th className="px-4 py-2">タイトル</th>
+                                <th className="px-4 py-2">記事</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                <td className="border px-4 py-2">{props.blogs}</td>
+                                <td className="border px-4 py-2"></td>
+                                <td className="border px-4 py-2"></td>
+                                </tr>
+                            </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+</div>
 
         </Authenticated>
     );
