bcef4a564c133c0746c4fd5bb249a1b4ae7ea045
boner.engineer <boner.engineer@outlook.jp>
2022-07-20 17:09:11 +0900

    日付表示修正

--- src/app/Models/User.php
+++ src/app/Models/User.php
@@ -58,4 +58,8 @@ class User extends Authenticatable
     protected $appends = [
         'profile_photo_url',
     ];
+    protected function serializeDate(\DateTimeInterface $date)
+    {
+        return $date->format('Y/m/d H:i:s');
+    }
 }
